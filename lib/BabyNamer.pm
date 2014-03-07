7561f29424ca4ea6e1be51106f1a86cb7f4c7692
Tyler Neu <tyler@tylerneu.com>
2014-03-06 22:35:06 -0600

    Bootstrap. Showing historical data.

--- lib/BabyNamer.pm
+++ lib/BabyNamer.pm
@@ -6,19 +6,14 @@ use Dancer::Plugin::Ajax;
 our $VERSION = '0.1';
 
 get '/' => sub {
-  
   session 'random_name_count' => 0;
-  
-  template 'index', {
-    name => get_random_name(),
-  };
-  
+  template 'index', {};
 };
 
 ajax '/random_name' => sub {
   my $count = session 'random_name_count';
   session 'random_name_count' => ++$count;
-  return to_json(get_random_name());
+  return to_json(get_random_name_data());
 };
 
 ajax '/random_name_count' => sub {  
@@ -27,8 +22,10 @@ ajax '/random_name_count' => sub {
 
 true;
 
-sub get_random_name {
-  my $sth = database->prepare('SELECT * FROM name JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM name)) AS id) AS r2 USING (id);');
+sub get_random_name_data {
+  my $sth = database->prepare('SELECT name.id, name.name, name.sex, score.year, SUM(score.score) as yearly_score FROM name JOIN (SELECT CEIL(RAND() * (SELECT MAX(name.id) FROM name)) AS id) AS r2 USING (id) JOIN `score` on name.id = score.name_id GROUP BY score.year;');
+    # my $sth = database->prepare('SELECT name.id, name.name, name.sex, score.year, SUM(score.score) as yearly_score FROM name JOIN (SELECT CEIL(RAND() * (SELECT MAX(name.id) FROM name)) AS id) AS r2 USING (id) JOIN `score` on name.id = score.name_id WHERE name.sex = "M" GROUP BY score.year;');
   $sth->execute();
-  return $sth->fetchrow_hashref;
+  my $name = $sth->fetchall_hashref('year');  
+  return $name;
 }
--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,21 +1,7 @@
 $(function() {
   
   $('button#get_random_name').click(function() {
-    
-    $.ajax({
-        url: "/random_name",
-        type: "GET",
-        dataType : "json",
-        success: function( json ) {
-            $('h1#name').text( json.name );
-        },
-        error: function( xhr, status ) {
-            alert( "Sorry, there was a problem!" );
-        },
-        complete: function( xhr, status ) {
-            // alert( "The request is complete!" );
-        }
-    });
+    show_random_name_data();
   });
   
   $('button#get_random_name_count').click(function() {
@@ -35,6 +21,27 @@ $(function() {
         }
     });
   });
-   
     
 });
+
+function show_random_name_data() {
+  $.ajax({
+      url: "/random_name",
+      type: "GET",
+      dataType : "json",
+      success: function( json ) {
+        $('#name_data').empty();
+        $.each(json, function(year) {
+          $('<li></li>', {
+            text: [json[year].name, json[year].sex, json[year].year, json[year].yearly_score].join(' : ')
+          }).appendTo($('#name_data'));
+        });
+      },
+      error: function( xhr, status ) {
+          alert( "Sorry, there was a problem!" );
+      },
+      complete: function( xhr, status ) {
+          // alert( "The request is complete!" );
+      }
+  });
+}
