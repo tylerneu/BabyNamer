37b2b27eafc31a9604179650b07f22304194c815
Tyler Neu <Tyler.Neu@icainformatics.com>
2014-05-20 23:18:42 -0500

    Resolves #12 & #7. Now using jquery-cookie as submodule

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,15 +1,13 @@
 $(function() {
   
+  $.cookie.json = true;
+  
   $('button#save_name').click(function() {
     save_name();
   });
   
   $('button#get_random_name').click(function() {
     show_random_name_data();
-  });
-  
-  $('button#get_random_name_count').click(function() {
-    get_random_name_count();
   });
   
   get_saved_names();
@@ -21,26 +19,30 @@ $(function() {
 // ========================================================================== //
 // ========================================================================== //
 
+function save_name() {
+  
+  var saved_names = $.cookie("saved_names") || [];
+  
+  // Only insert if it doesn't exist in the list
+  var current_name = $( "body" ).data( "current_name");
+  if ($.inArray(current_name, saved_names) == -1) saved_names.push(current_name);
+
+  $.cookie("saved_names", saved_names);
+  
+  // Refresh list
+  get_saved_names();
+  
+}
+
 function get_saved_names() {
-  $.ajax({
-      url: "/saved_names",
-      type: "GET",
-      dataType : "json",
-      success: function( json ) {
-        $('#saved_names').empty();
-        $.each(json.saved_names, function(index, name_text) {
-          $('<li></li>', {
-            text: name_text
-          }).appendTo($('#saved_names'));
-        });  
-      },
-      error: function( xhr, status ) {
-          alert( "Sorry, there was a problem!" );
-      },
-      complete: function( xhr, status ) {
-          // alert( "The request is complete!" );
-      }
-  });
+  
+  $('#saved_names').empty();
+  $.each($.cookie("saved_names"), function(index, name_text) {
+    $('<li></li>', {
+      text: name_text
+    }).appendTo($('#saved_names'));
+  });  
+  
 }
 
 function show_random_name_data() {
@@ -51,45 +53,17 @@ function show_random_name_data() {
       dataType : "json",
       
       success: function( json ) {
+        
+        // Save current name for saving
+        $( "body" ).data( "current_name", json.current_name );
+        
         $('#name_data').empty();
-        $.each(json, function(year) {
+        $.each(json.years, function(year) {
+          var y = json.years[year];
           $('<li></li>', {
-            text: [json[year].name, json[year].sex, json[year].year, json[year].yearly_score].join(' : ')
+            text: y.name, y.sex, y.year, json.years[year].yearly_score].join(' : ')
           }).appendTo($('#name_data'));
         });
-      },
-      error: function( xhr, status ) {
-          alert( "Sorry, there was a problem!" );
-      },
-      complete: function( xhr, status ) {
-          // alert( "The request is complete!" );
-      }
-  });
-}
-
-function save_name() {
-  $.ajax({
-      url: "/save_name",
-      type: "POST",
-      success: function( json ) {
-        console.log(json);
-      },
-      error: function( xhr, status ) {
-          alert( "Sorry, there was a problem!" );
-      },
-      complete: function( xhr, status ) {
-        get_saved_names();
-      }
-  });
-}
-
-function get_random_name_count() {
-  $.ajax({
-      url: "/random_name_count",
-      type: "GET",
-      dataType : "json",
-      success: function( json ) {
-        console.log(json);
       },
       error: function( xhr, status ) {
           alert( "Sorry, there was a problem!" );
