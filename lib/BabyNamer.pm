3634d1147508e9466b2971dd0d972eebdf6e0278
Tyler Neu <Tyler.Neu@icainformatics.com>
2014-07-30 22:01:52 -0500

    Resolves #30, RESTful API to search and dive

--- lib/BabyNamer.pm
+++ lib/BabyNamer.pm
@@ -24,4 +24,101 @@ ajax '/random_name' => sub {
   return to_json($data);
 };
 
+get '/name/:id' => sub {
+  
+  my $sth = database->prepare('SELECT name.id, name.name, name.sex, score.year, SUM(score.score) as yearly_score, name.recent_total_names FROM name JOIN `score` on name.id = score.name_id WHERE name.id = ? GROUP BY id, score.year;');
+  $sth->execute(params->{id});
+  my $data = $sth->fetchall_hashref(['year']);
+
+  $data = {
+    name => $data->{(keys %$data)[0]}->{name},
+    sex  => $data->{(keys %$data)[0]}->{sex},   
+    years => $data,
+  };
+  
+  template 'name.tt', $data, { layout => undef };
+  
+};
+
+# SEARCH
+
+get '/names' => sub {
+  
+  my $sth = database->prepare("SELECT name.id, name.name, name.sex FROM name WHERE name LIKE ? ORDER BY name DESC;");
+  $sth->execute(params->{query});
+  my $data = $sth->fetchall_hashref(['id']);
+  
+  template 'names.tt', { names => $data }, { layout => undef };
+  
+};
+
+# POPULAR NAMES BY YEAR
+
+get '/year/:year' => sub {
+  
+  my $year = params->{year};
+  
+  my $sth = database->prepare("SELECT name.id, name.name, name.sex, score.year, SUM(score.score) as year_score
+                              FROM score
+                              JOIN name ON score.name_id = name.id
+                              WHERE score.year = ?
+                              group by score.name_id, score.year
+                              ORDER BY year_score DESC
+                              LIMIT 100;");
+  $sth->execute($year);
+  my $data = $sth->fetchall_hashref(['id']);
+  
+  my @sorted_data = map { $data->{$_} } sort { $data->{$b}->{year_score} <=> $data->{$a}->{year_score} } keys %$data;
+  
+  debug Dumper(\@sorted_data);
+  
+  template 'names.tt', { variable => $year, names => \@sorted_data }, { layout => undef };
+  
+};
+
+# POPULAR NAMES BY STATE 
+
+get '/state/:state' => sub {
+  
+  my $state = params->{state};
+  
+  my $sth = database->prepare("SELECT name.id, name.name, name.sex, score.year, SUM(score.score) as state_score
+                              FROM score
+                              JOIN name ON score.name_id = name.id
+                              WHERE score.state = ?
+                              group by score.name_id, score.state
+                              ORDER BY state_score DESC
+                              LIMIT 100;");
+  $sth->execute($state);
+  my $data = $sth->fetchall_hashref(['id']);
+  
+  my @sorted_data = map { $data->{$_} } sort { $data->{$b}->{state_score} <=> $data->{$a}->{state_score} } keys %$data;
+  
+  template 'names.tt', { variable => $state, names => \@sorted_data }, { layout => undef };
+  
+};
+
+# POPULAR NAMES BY YEAR & STATE
+
+get '/year/:year/state/:state' => sub {
+  
+  my $year = params->{year};
+  my $state = params->{state};
+  
+  my $sth = database->prepare("SELECT name.id, name.name, name.sex, score.year, SUM(score.score) as state_score
+                              FROM score
+                              JOIN name ON score.name_id = name.id
+                              WHERE score.year = ? AND score.state = ?
+                              group by score.name_id
+                              ORDER BY state_score DESC
+                              LIMIT 100;");
+  $sth->execute($year, $state);
+  my $data = $sth->fetchall_hashref(['id']);
+  
+  my @sorted_data = map { $data->{$_} } sort { $data->{$b}->{state_score} <=> $data->{$a}->{state_score} } keys %$data;
+  
+  template 'names.tt', { variable => "$year in $state", names => \@sorted_data }, { layout => undef };
+  
+};
+
 true;
